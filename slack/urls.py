bea523a937c8390b3dabbdedf872a8118a47d9d4
Nick Sullivan <nick@sullivanflock.com>
2014-12-17 12:00:01 -0800

    initial django project

---
+++ slack/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'slack.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+)
